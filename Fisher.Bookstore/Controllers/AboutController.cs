cc1c9bc93f9de6d87f1ddb1c6405d1b8b15a4508
Michael <michaeltrotta@outlook.com>
2019-02-05 19:36:15 -0500

    lab 3 changes

--- Fisher.Bookstore/Controllers/AboutController.cs
+++ Fisher.Bookstore/Controllers/AboutController.cs
@@ -7,17 +7,17 @@ namespace Fisher.Bookstore.Controllers
     {
         public IActionResult Index()
         {
-            return Content("This is the About controller's Index action.");
+            return View();
         }
 
         public IActionResult History()
         {
-            return Content("This is the About controller's History action.");
+            return View();
         }
         
         public IActionResult Location()
         {
-            return Content("This is the About controller's Location action.");
+            return View();
         }
     }
 }
--- Fisher.Bookstore/Controllers/BooksController.cs
+++ Fisher.Bookstore/Controllers/BooksController.cs
@@ -3,22 +3,26 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Fisher.Bookstore.Controllers
 {
+    [Route("books")]
     public class BooksController : Controller
     {
+        [Route("")]
+        [Route("index")]
         public IActionResult Index()
         {
-            return Content("This is the Books controller's default action.");
+            return View();
         }
 
+        [Route("new")]
         public IActionResult New()
         {
-            return Content("This is the Books controller's New action.");
+            return View();
         }
         
-        [ActionName("Best-Sellers")]
+        [Route("best-sellers")]
         public IActionResult Best()
         {
-            return Content("This is the Books controller's Best-Sellers action.");
+            return View();
         }
     }
 }
